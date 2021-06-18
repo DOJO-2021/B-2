0d00848e5c54014ac60574dba38ee28e1c6df9a9
DOJO2021-y-yoshimura <yoshimura@seplus2016.onmicrosoft.com>
2021-06-18 17:03:50 +0900

    yoshimura

--- CAP/src/servlet/S_ViewServlet.java
+++ CAP/src/servlet/S_ViewServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.Browsing_BDao;
 import dao.Browsing_CDao;
@@ -30,25 +31,25 @@ public class S_ViewServlet extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	//	HttpSession session = request.getSession();
-	//	if (session.getAttribute("user-id") == null) {
-	//		response.sendRedirect("/CAP/S_LoginServlet");
-	//		return;
-	//	}
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/CAP/S_LoginServlet");
+			return;
+		}
 
-
+		//投稿内容を全検索
 		PostDao pDao = new PostDao();
 		List<Post> PostList = pDao.postSelectAll(new Post());
 		request.setAttribute("PostList", PostList);
-
+		//ジャンル内容を全検索
 		GenreDao gDao = new GenreDao();
 		List<Genre> GenreList = gDao.genleSerectAll(new Genre());
 		request.setAttribute("GenreList", GenreList);
-
+		//投稿のスタンプを全検索
 		Browsing_BDao sDao = new Browsing_BDao();
 		List<Browsing_B> StampList = sDao.stampSelectAll(new Browsing_B());
 		request.setAttribute("StampList", StampList);
-
+		//コメント内容を全検索
 		Browsing_CDao cDao = new Browsing_CDao();
 		List<Browsing_C> CommentList = cDao.commentSelectAll(new Browsing_C());
 		request.setAttribute("CommentList", CommentList);
@@ -58,48 +59,48 @@ public class S_ViewServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		HttpSession session = request.getSession();
-//		if (session.getAttribute("user-id") == null) {
-//			response.sendRedirect("/CAP/LoginServlet");
-//			return;
-//		}
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user_id") == null) {
+			response.sendRedirect("/CAP/S_LoginServlet");
+			return;
+		}
 
 		request.setCharacterEncoding("UTF-8");
-		String id = request.getParameter("post_id");
+		//コメント内容を保存する
 		String comment = request.getParameter("comment");
-//		Strinig user_id = session.getAttribute("user-id");
-
-		int user_id = 1;
+		//ユーザーIDをセッションから持ってくる
+		Object obj = session.getAttribute("user_id");
+		String user = obj.toString();
+		int user_id = Integer.parseInt(user);
+		//コメント投稿時刻を保存する
 		Calendar calendar = Calendar.getInstance();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm:ss");
 		String str = sdf.format(calendar.getTime());
 		String str1 = sdf1.format(calendar.getTime());
-		System.out.println(str);
-		System.out.println(str1);
 		Date date = Date.valueOf(str);
 		Time time = Time.valueOf(str1);
-
+		//ポストIDをintに変換して保存
+		String id = request.getParameter("post_id");
 		int Id = Integer.parseInt(id);
-//		Date Date = Date.valueOf(date);
-//		Time Time = Time.valueOf(time);
-//		int User_id = Integer.parseInt(user_id);
 
+		//コメントをＤＢに記録する
 		Browsing_CDao cDao = new Browsing_CDao();
 		if (cDao.commentInsert(new Browsing_C(0,Id,comment,date,time,user_id))) {
 
+			//投稿内容を全検索
 			PostDao pDao = new PostDao();
 			List<Post> PostList = pDao.postSelectAll(new Post());
 			request.setAttribute("PostList", PostList);
-
+			//ジャンル内容を全検索
 			GenreDao gDao = new GenreDao();
 			List<Genre> GenreList = gDao.genleSerectAll(new Genre());
 			request.setAttribute("GenreList", GenreList);
-
+			//投稿のスタンプを全検索
 			Browsing_BDao sDao = new Browsing_BDao();
 			List<Browsing_B> StampList = sDao.stampSelectAll(new Browsing_B());
 			request.setAttribute("StampList", StampList);
-
+			//コメント内容を全検索
 			Browsing_CDao bcDao = new Browsing_CDao();
 			List<Browsing_C> CommentList = bcDao.commentSelectAll(new Browsing_C());
 			request.setAttribute("CommentList", CommentList);
